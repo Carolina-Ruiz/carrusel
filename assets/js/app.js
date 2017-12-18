d959e9513d6becbcf98b9f48582f4a04ada8d286
agustinaVal <titivalenti24@gmail.com>
2017-12-18 12:54:00 -0300

    carrusel de imagenes

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){	
+	$("#slider").easySlider({
+		auto: true,
+		continuous: true,
+		nextId: "slider1next",
+		prevId: "slider1prev"
+	});
+	$("#slider2").easySlider({ 
+		numeric: true
+	});
+});
